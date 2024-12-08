4a4c59f194bb8de0ca374c83830ce8719a5a48b1
Dominick Faust <kingkongdom@comcast.net>
2024-12-08 10:39:29 -0500

    removed comment

--- footer.js
+++ footer.js
@@ -1,5 +1,4 @@
 // Load shared footer content dynamically
 document.getElementById('footer-placeholder').innerHTML = `
     <p>&copy; 2024 OSE Helpers | All Rights Reserved</p>
-    <!--<p>Contact us at <a href="mailto:info@example.com">info@example.com</a></p>-->
 `;
