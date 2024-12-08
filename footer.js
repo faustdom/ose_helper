16ffb0d8ab5f726dedfae2da099ffc68c48c5753
Dominick Faust <kingkongdom@comcast.net>
2024-12-08 10:37:46 -0500

    Added style formats and home page

---
+++ footer.js
@@ -0,0 +1,5 @@
+// Load shared footer content dynamically
+document.getElementById('footer-placeholder').innerHTML = `
+    <p>&copy; 2024 OSE Helpers | All Rights Reserved</p>
+    <!--<p>Contact us at <a href="mailto:info@example.com">info@example.com</a></p>-->
+`;
